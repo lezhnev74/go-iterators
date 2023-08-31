ca6711177d55769453b9f4afe049a12abfa11f65
lezhnev <lezhnev.work@gmail.com>
2023-08-31 23:16:00 +0200

    add MappingIterator

---
+++ mapping_iterator.go
@@ -0,0 +1,22 @@
+package go_iterators
+
+type MappingIterator[T any, InnerT any] struct {
+	innerIterator Iterator[InnerT]
+	mf            func(InnerT) T
+}
+
+func (m *MappingIterator[T, InnerT]) Next() (T, error) {
+	v, err := m.innerIterator.Next()
+	return m.mf(v), err
+}
+
+func (m *MappingIterator[T, InnerT]) Close() error {
+	return m.innerIterator.Close()
+}
+
+func NewMappingIterator[T any, InnerT any](inner Iterator[InnerT], mf func(InnerT) T) Iterator[T] {
+	return &MappingIterator[T, InnerT]{
+		innerIterator: inner,
+		mf:            mf,
+	}
+}
---
+++ mapping_iterator_test.go
@@ -0,0 +1,18 @@
+package go_iterators
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMappingIterator(t *testing.T) {
+	inner := NewSliceIterator([]int{1, 2, 3, 4, 5})
+	mp := func(i int) string {
+		return fmt.Sprintf("%d", i*10)
+	}
+
+	mIt := NewMappingIterator(inner, mp)
+	out := ToSlice(mIt)
+	require.EqualValues(t, []string{"10", "20", "30", "40", "50"}, out)
+}
