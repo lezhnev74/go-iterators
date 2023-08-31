e7f7169ff7a3f39bd95e5987794791804ca1cf37
lezhnev <lezhnev.work@gmail.com>
2023-08-31 23:01:06 +0200

    add FilteringIterator

---
+++ filtering_iterator_test.go
@@ -0,0 +1,18 @@
+package go_iterators
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestFilteringIterator(t *testing.T) {
+
+	inner := NewSliceIterator([]int{1, 2, 3, 4, 5})
+	filter := func(i int) bool {
+		return i%2 == 0
+	}
+
+	filteringIt := NewFilteringIterator(inner, filter)
+	out := ToSlice(filteringIt)
+	require.EqualValues(t, []int{2, 4}, out)
+}
